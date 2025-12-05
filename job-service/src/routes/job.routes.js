8392d8362976dd948982bd7fb56da06d09e7b863
coder-nitin07 <nitinthakur87504@gmail.com>
2025-12-05 16:28:31 +0530

    re-setup everything form scratch

--- job-service/src/routes/job.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { createJob } = require('../controllers/job.controller');
-const jobRouter = express.Router();
-
-jobRouter.post('/createJob', createJob);
-
-module.exports = jobRouter;
