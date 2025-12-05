1707bb1190ea0c20fcc2c2ec70fe50dd69e004e1
coder-nitin07 <nitinthakur87504@gmail.com>
2025-12-05 19:46:24 +0530

    scheam done

---
+++ api-gateway/src/validation/job.schema.js
@@ -0,0 +1,21 @@
+const Joi = require('joi');
+
+const createJobSchema = Joi.object({
+    type: Joi.string().required(),
+    payload: Joi.string().required(),
+    maxAttempts: Joi.number().integer().min(1).default(3)
+});
+
+function validate(schema){
+    return (req, res, next) => {
+        const { error, value } = schema.validate(req.body, { stripUnknown: true, abortEarly: false });
+        if(error){
+            return res.status(400).json({ error: error.details.map(d => d.message).join(', ') });
+        }
+
+        req.body = value;
+        next();
+    };
+}
+
+exports.validateCreateJob = validate(createJobSchema);
