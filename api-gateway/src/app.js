3ec0f1565946bb6e6362f9f86af7b7a6ebbd21ba
coder-nitin07 <nitinthakur87504@gmail.com>
2025-12-06 12:28:11 +0530

    Setup Apo.js file

--- api-gateway/src/app.js
+++ api-gateway/src/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const { route } = require('./routes');
+const notfoundMiddleware = require('./middlewares/notfound.middleware');
+const errorMiddleware = require('./middlewares/error.middleware');
 
 const createApp = () =>{
   const app = express();
@@ -11,6 +14,19 @@ const createApp = () =>{
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(morgan('dev'));
+
+  // routes
+  app.use('/api', route);
+
+  // error handler
+  app.use(notfoundMiddleware);
+  app.use(errorMiddleware);
+
+  // test route
+  app.get('/', (req, res)=>{
+    res.json({ ok: true, service: 'api-gateway' });
+  });
+
 };
 
 module.exports = createApp;
