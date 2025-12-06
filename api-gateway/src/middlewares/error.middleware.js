9e7a444a30cece2c044742008e0770c39477468f
coder-nitin07 <nitinthakur87504@gmail.com>
2025-12-06 11:47:02 +0530

    Add Middlewares in api-gateway

---
+++ api-gateway/src/middlewares/error.middleware.js
@@ -0,0 +1,9 @@
+const logger = require('../utils/logger');
+
+module.exports = (err, req, res, next)=>{
+    logger.error(err);
+
+    const status = err.status || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(status).json({ error: message });
+};
